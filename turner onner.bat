ed8448912987ed8829745ca963c5eafbd03beda7
wrath3k16 <71242861+wrath3k16@users.noreply.github.com>
2021-07-18 14:24:45 -0500

    Update turner onner.bat

--- turner onner.bat
+++ turner onner.bat
@@ -4,7 +4,6 @@ title Failsafe Launcher
 git fetch --all
 git reset --hard
 git reset --hard HEAD
-git clean -f -d
 git pull https://github.com/wrath3k16/other-selfbot.git
 py -3.8 Failsafe.py
 echo stfu nigger
