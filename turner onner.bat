77fa35da84554b31a8232eeac0f01ea01df238e2
wrath3k16 <71242861+wrath3k16@users.noreply.github.com>
2021-08-08 21:05:23 -0500

    Update turner onner.bat

--- turner onner.bat
+++ turner onner.bat
@@ -1,10 +1,5 @@
 @echo off
 cls
 title Failsafe Launcher
-git init
-git fetch --all
-git reset --Failsafe.py
-git reset --hard HEAD
-git pull https://github.com/wrath3k16/other-selfbot.git
 py -3.8 Failsafe.py
 pause
